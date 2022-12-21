b84cf03ae35daf3bcd21b6adffa7631afc480f84
John Rooney <rooneyjohn@gmail.com>
2022-12-21 00:00:32 -0500

    Commented everything for notes

--- src/index.js
+++ src/index.js
@@ -1,104 +1,87 @@
 let addToy = false;
-const toyCollection = document.querySelector("#toy-collection");
-const toyform = document.querySelector(".add-toy-form");
-const url = `http://localhost:3000/toys`
+const toyCollection = document.querySelector("#toy-collection");    // Selects HTML item by ID
+const toyForm = document.querySelector(".add-toy-form");            // Selects HTML item by class
+const url = `http://localhost:3000/toys`                            // Creates variable for URL
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  const newToySubmit = document.querySelector(".add-toy-form");
+document.addEventListener("DOMContentLoaded", () => {               // Causes things to wait for DOM to load
+  const addBtn = document.querySelector("#new-toy-btn");            // Selects button by ID
+  const toyFormContainer = document.querySelector(".container");    // selects container by class
+  const newToySubmit = document.querySelector(".add-toy-form");     // selects submit button by class
 
-  newToySubmit.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(e.target.name.value);
-    console.log(e.target.image.value);
-  })
-
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
+  addBtn.addEventListener("click", () => {        // hide & seek with the form provided by Flatiron
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
     } else {
       toyFormContainer.style.display = "none";
     }
-  });
-  //Get all toys and populate page
-  fetchALL();
-  //add on form submit to toyform
-  toyform.addEventListener('submit', onFormSubmit)
+  });                                             // END CODE hide & seek with the form provided by Flatiron
+  
+  fetchALL();                                         //Get all toys and populate page
+  toyForm.addEventListener('submit', onFormSubmit)    //add on form submit to toyform
 });
 
-//fetch toys (GET)
-const fetchALL = () => {
-  fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    //ADD TOY CARD
-    data.forEach(character => {
-      addToyCard(character)
-      
+const fetchALL = () => {  //fetch toys (GET)
+  fetch(url)                              // Fetch from http://localhost:3000/toys
+  .then((response) => response.json())    // Turns the resonse from previous line into an object
+  .then((data) => {                       // Passes the object from previous line as 'data' so we can use it
+    data.forEach(character => {           // Loop through each toy
+      addToyCard(character)               // Adds a card for every character
     });
   })
 }
-//Add Toy Card
-const addToyCard = (character) => {
-  //Create all elements
-  let div = document.createElement("div");
-  let h2 = document.createElement("h2");
-  let img = document.createElement("img");
-  let p = document.createElement("p");
-  let button = document.createElement("button");
+
+const addToyCard = (character) => { //Add Toy Card
+  let div = document.createElement("div");          //Create all elements
+  let h2 = document.createElement("h2");            //Create all elements
+  let img = document.createElement("img");          //Create all elements
+  let p = document.createElement("p");              //Create all elements
+  let button = document.createElement("button");    //Create all elements
   
-  //populate elements
-  div.classList.add("card");
-  h2.innerText = character.name;
-  img.src = character.image;
-  img.classList.add('toy-avatar')
-  p.innerText = `${character.likes} Likes`
-  button.innerText = "Like ❤️";
-  button.id = `${character.id}`
-  button.addEventListener('click', (e) => {
-    onButtonClicked(e)
+  
+  div.classList.add("card");              // Adds the class 'card' to the div
+  h2.innerText = character.name;          // Sets H2 inner Text to character name passed into from fetchALL()
+  img.src = character.image;              // Sets Image src to character image passed into from fetchALL()
+  img.classList.add('toy-avatar')         // Adds the class 'toy-avatar' to the img
+  p.innerText = `${character.likes} Likes`     // Sets p inner Text to character likes passed into from fetchALL()
+  button.innerText = "Like ❤️";               // Sets button inner Text to 'Like ❤️'
+  button.id = `${character.id}`               // Sets button id to character id passed into from fetchALL()
+  
+  button.addEventListener('click', (e) => {   // add event listener for each new Card
+    onLikedButtonClicked(e)                        // Passes the event to onButtonClicked()
   })
   
-  //put card/elements in dom
-  div.appendChild(h2);
-  div.appendChild(img);
-  div.appendChild(p);
-  div.appendChild(button);
-  toyCollection.appendChild(div);
+  div.appendChild(h2);              //put element in dom
+  div.appendChild(img);             //put element in dom
+  div.appendChild(p);               //put element in dom
+  div.appendChild(button);          //put element in dom
+  toyCollection.appendChild(div);   //put element in dom
 }
 
-//on form submit
-const onFormSubmit = (e) => {
-  //get form information
-    e.preventDefault();
-    let formData = {
-      name: e.target.name.value,
-      image: e.target.image.value,
-      likes: 0
-    }
+const onFormSubmit = (e) => { //on form submit
+  e.preventDefault();                 // prevents default form submit behaviour
+  let formData = {                    // creates formData object (keys/values)
+    name: e.target.name.value,        // name value(from input)
+    image: e.target.image.value,      // image value(from input)
+    likes: 0                          // Sets likes to base value of 0
+  }
 
-  //add a new toy (POST)
-  fetch(`${url}`, {
-    method: "POST",
-    body: JSON.stringify(formData),
-    headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        }
-    })
-    .then(res => res.json())
-    .then(data => {
-      //ADD toy card
-      addToyCard(data)
-    })
+  
+  fetch(`${url}`, {   //add a new toy (POST)
+    method: "POST",                         // The method is set to POST
+    headers: {                              // Creates the headers
+      "Content-Type": "application/json",   // I guess you need this for JSONS
+      "Accept": "application/json"          // I guess you need this for JSONS
+    },                                      
+    body: JSON.stringify(formData)          // The body is what we want to add when using POST
+  })
+  .then(res => res.json())                  // Returns newly updated JSON and converts it to an object
+  .then(data => {                           // Passes in object from line above
+    addToyCard(data)                        // Adds the new toy to the DOM (this is the pesimistic way)
+  })
 }
 
-//On like button click
-const onButtonClicked = (e) => {
-  //get toy id
+const onLikedButtonClicked = (e) => {  //On liked button click
   let id = e.target.id
   let likesText = e.target.previousElementSibling
   let numLikes = parseInt(likesText.innerText.split(" ")[0]) + 1
